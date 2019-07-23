b14b00fd732341d98c1fc9fb9a6b3aacc1de514f
Alex Martin <amartin@excilys.com>
2019-07-23 12:23:41 +0200

    [REVERT] Revert "Using Error Boundaries" to Continue Course

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
     state = {
@@ -54,14 +53,13 @@ class App extends Component {
                 <div>
                     {
                         this.state.persons.map((person, index) => {
-                            return <ErrorBoundary key={person.id}>
-                                <Person
+                            return <Person
                                 name={person.name}
                                 age={person.age}
                                 click={() => this.deletePersonHandler(index)}
+                                key={person.id}
                                 changed={(event) => this.nameChangedHandler(event, person.id)}
                             />
-                            </ErrorBoundary>
                         })
                     }
                 </div>
