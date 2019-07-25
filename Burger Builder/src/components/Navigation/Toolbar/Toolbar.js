776ee9fbe99ca2cf14e36620222206d8af49a5ff
Alex Martin <amartin@excilys.com>
2019-07-25 15:55:33 +0200

    [COMPONENT] Adding Reusable Navigation Items

--- Burger Builder/src/components/Navigation/Toolbar/Toolbar.js
+++ Burger Builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 
 const Toolbar = props => {
@@ -8,9 +9,7 @@ const Toolbar = props => {
             <header className={classes.Toolbar}>
                 <div>MENU</div>
                 <Logo />
-                <nav>
-                    ...
-                </nav>
+                <NavigationItems />
             </header>
     );
 };
