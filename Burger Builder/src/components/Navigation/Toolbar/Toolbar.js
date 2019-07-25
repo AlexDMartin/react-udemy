3989965e52cb0b2ddfb068eff022a3a3be894b6e
Alex Martin <amartin@excilys.com>
2019-07-25 15:39:34 +0200

    [COMPONENT] Adding a Logo in Our Application

--- Burger Builder/src/components/Navigation/Toolbar/Toolbar.js
+++ Burger Builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 
 const Toolbar = props => {
     return (
-        <div className={classes.Toolbar}>
-            <header>
+            <header className={classes.Toolbar}>
                 <div>MENU</div>
-                <div>LOGO</div>
+                <Logo />
                 <nav>
                     ...
                 </nav>
             </header>
-        </div>
     );
 };
 
