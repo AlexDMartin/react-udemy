4f32158e753ea2e22dbb56a2e92923e2972ffbff
Alex Martin <amartin@excilys.com>
2019-07-26 11:29:55 +0200

    [GET] Creating a HTTP Request to Get Data

--- Post App/src/containers/Blog/Blog.js
+++ Post App/src/containers/Blog/Blog.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(result => {
+                console.log('result', result);
+            });
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    <Post/>
+                    <Post/>
+                    <Post/>
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
