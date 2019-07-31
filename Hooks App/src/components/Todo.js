4d705249ab8f3144d97e93e891dab69af83a722a
Alex Martin <amartin@excilys.com>
2019-07-31 16:26:51 +0200

    [HOOKS] The UseState Hook

---
+++ Hooks App/src/components/Todo.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+const Todo = props => {
+    const inputState = useState('');
+
+    const inputChangedHandler = (event) => {
+        inputState[1](event.target.value);
+    };
+
+    return (
+        <React.Fragment>
+            <input
+                type="text"
+                placeholder="Todo"
+                onChange={inputChangedHandler}
+                value={inputState[0]}
+            />
+            <button type="button">Add</button>
+            <ul/>
+        </React.Fragment>
+    );
+};
+
+
+export default Todo;
