ab40b8596a28b784693a5bdeedd0986ad9443cf6
Alex Martin <amartin@excilys.com>
2019-07-31 11:07:19 +0200

    [REDUX] Adding Middleware

--- Redux App/src/index.js
+++ Redux App/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 import {Provider} from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -12,7 +12,19 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-const store = createStore(rootReducer);
+
+const logger = (store) => {
+    return (next) => {
+        return (action) => {
+          console.log('[Middleware] Dispatching', action);
+          const result = next(action);
+          console.log('[Middleware] Next State', store.getState());
+          return result;
+        };
+    };
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
